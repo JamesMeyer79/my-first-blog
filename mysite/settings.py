b2dd677b9d3fc59d83b1cf5a5df4643dc2636865
JamesMeyer79 <jmeyer154@ivytech.edu>
2025-09-27 22:55:31 -0500

    Settings fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,4 +122,4 @@ STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-ADEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
